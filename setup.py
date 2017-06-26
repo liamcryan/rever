5d7ee83e63c445b5995712ccb4a671b798ab295d
Liam Cryan <limecrayon@gmail.com>
2017-06-25 19:33:40 -0700

    added to setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(name=about["__title__"],
       packages=[about["__title__"]],
       keywords="retry decorator rever",
       classifiers=["Development Status :: 2 - Pre-Alpha",
+                   "Intended Audience :: Developers",
                    "Programming Language :: Python 3",
                    "Programming Language :: Python 3.5"]
       )
